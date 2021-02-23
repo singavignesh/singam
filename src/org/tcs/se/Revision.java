53b3f3627ed86434b3816eb5095a3a85bc67b5c2
singavignesh <k.vignesh28@gmail.com>
2021-02-23 22:46:54 +0530

    inital2 commit

--- src/org/tcs/se/Revision.java
+++ src/org/tcs/se/Revision.java
@@ -8,6 +8,8 @@ public class Revision {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vignesh\\eclipse\\workspace\\RevisionSelenium\\Webdriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
+		
+		System.out.println("Facebook URl entered");
 	}
 	public static void main(String[] args) {
 		Revision st = new Revision();
